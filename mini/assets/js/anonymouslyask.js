fe2aa5eca73e805b17b057f33d7a565625849cba
sm <contact.smtanvir@gmail.com>
2023-10-08 13:26:28 +0600

    first commit

--- mini/assets/js/anonymouslyask.js
+++ mini/assets/js/anonymouslyask.js
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 function sendEmail() {
     Email.send({
       SecureToken: "0c816fe8-5970-40be-a50f-bdb07b6d9860",
@@ -5,8 +11,8 @@ function sendEmail() {
       From: "mboxreply@gmail.com",
       Subject: "Anonymously Asked :) ",
       Body: 
-             "Hi! You have received an anonymous messege from Mailbox | Anonymous - <br>" 
-             +"<br> <b>Message to you:</b> <br>" + document.getElementById("msg").value 
+             "Hi! You have received an anonymous messege from Mailbox | Anonymous - " 
+             +"<br> <br><br> <b>Message to you:</b> <br>" + document.getElementById("msg").value +"<br><br><br><br><br><br>"
     })
     .then(
       // message => alert("Your messege sent successfully ! Thanks for your question.")
