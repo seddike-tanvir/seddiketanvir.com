276c5072616d210d11e419040757e4a0e6a52ae9
sm <contact.smtanvir@gmail.com>
2023-12-03 03:56:13 +0600

    bug_fixed_03/12/2023_

--- mini/blogs/assets/js/app.js
+++ mini/blogs/assets/js/app.js
@@ -1,36 +1,129 @@
-// let passCode = document.querySelector('.pass_box');
-// /let formArea = document.querySelector('.form_area');
-// let contentArea = document.querySelector('.content_area');
-
-// let passCode = prompt('Enter Passcode to continue...');
-// if (passCode = 2025) {
-//         contentArea.style.display='flex';
-//             } else { 
-//                 contentArea.style.display='none';
-//                 alert("Wrong Passcode enterd. Try Again...");
-//             }
+let popupWindowBG = document.querySelector('.popup_window_bg');
+let xBtn = document.querySelector('.X');
+let messegeWindow = document.querySelector('.messege_popup');
+let xMessegeWindow = document.querySelector('.X_messege_popup');
+let preLoader = document.querySelector('.pre-loader');
 
 
 
 
 
+// --Pre-Loader -----
+
+window.addEventListener("load", function(){
+preLoader.style.display = "none";
+})
 
 
 
-
-
-// function loginCheck(){
-//     if (passCode == 2025) {
-//         contentArea.style.display='flex';
-//         formArea.style.display='none';
-//         alert("Mail Send successfully ! Thanks for using MailBox...");
-    
-//     } else {
-//         formArea.style.display='block';
-//         contentArea.style.display='none';
-//         errMsg.style.display='block';
-//         alert("...")
-    
+// //-----Light theme as default------//
+//   ui_icon.onclick = function(){
+//     document.body.classList.toggle('light_theme');
+//     if(document.body.classList.contains('light_theme')){
+//       document.querySelector('#img').src = 'assets/files/sun.png';           
+//     }else{
+//       document.querySelector('#img').src = 'assets/files/moon.png';
 
 //     }
+//   }
+
+//-----Dark theme as default------//
+// ui_icon.onclick = function(){
+// document.body.classList.toggle('light_theme');
+// if(document.body.classList.contains('light_theme')){
+//   document.querySelector('#img').src = 'assets/files/moon.png';
+// }else{
+//   document.querySelector('#img').src = 'assets/files/sun.png';
 // }
+// }
+
+
+
+//-----Menu Area-----//
+xMessegeWindow.onclick = function(){
+    popupWindowBG.style.display='none';
+    messegeWindow.style.display='none';
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// menu_icon.onclick = function(){
+// fullWindow.style.display='block'; 
+// xBtn.style.display= 'none';
+// menuBar.style.display='block';
+// }
+
+
+// xMenuBarBtn.onclick = function(){
+// fullWindow.style.display='none'; 
+// menuBar.style.display='none';
+// }
+
+// let typed = new Typed('#element', {
+// strings: [' Learner .', ' Web Devloper .', ' Tech Researcher .'],
+// typeSpeed: 150,
+// backSpeed: 150,
+// loop: true,
+// });
+// expandLinks.onclick = function(){
+//   expandedLinkBox.style.display='flex'; 
+//   expandLinks.style.display='none';
+//   minimizeLinks.style.display='block';
+// }
+// minimizeLinks.onclick = function(){
+//   expandedLinkBox.style.display='none'; 
+//   expandLinks.style.display='block';
+//   minimizeLinks.style.display='none';
+// }
+// contactBtn.onclick = function(){
+//   fullWindow.style.display='block'; 
+//   xBtn.style.display='block';
+//   contactWindow.style.display='block';
+// }
+// xBtn.onclick = function(){
+//   fullWindow.style.display='none';
+//   contactWindow.style.display='none';
+//   mailWindow.style.display='none';
+//   askWindow.style.display='none';
+
+// }
+
+// copyBtn.onclick = function(){
+//   input.select();
+//   document.execCommand('copy');
+//   alart.style.display='block';
+// }
+
