d2a7078bd02ee49135dff4d02cffc0b2020f6c54
“sm” <contact.smtanvir@gmail.com>
2024-09-16 02:26:25 +0600

    Commited by SM

---
+++ mini/lws/js/app.js
@@ -0,0 +1,36 @@
+// let passCode = document.querySelector('.pass_box');
+// /let formArea = document.querySelector('.form_area');
+// let contentArea = document.querySelector('.content_area');
+
+// let passCode = prompt('Enter Passcode to continue...');
+// if (passCode = 2025) {
+//         contentArea.style.display='flex';
+//             } else { 
+//                 contentArea.style.display='none';
+//                 alert("Wrong Passcode enterd. Try Again...");
+//             }
+
+
+
+
+
+
+
+
+
+
+// function loginCheck(){
+//     if (passCode == 2025) {
+//         contentArea.style.display='flex';
+//         formArea.style.display='none';
+//         alert("Mail Send successfully ! Thanks for using MailBox...");
+    
+//     } else {
+//         formArea.style.display='block';
+//         contentArea.style.display='none';
+//         errMsg.style.display='block';
+//         alert("...")
+    
+
+//     }
+// }
